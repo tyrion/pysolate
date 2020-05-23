2964efc3b78c57dcfce6876a6e5d62fd7e15633f
Germano Gabbianelli <git@germano.dev>
2020-05-23 17:00:50 +0200

    Rewrite pysolate.
    * Add support for Python < 3.3 by using virtualenv instead of venv
    * Add support for specifying required Python version

--- pysolate.py
+++ pysolate.py
@@ -1,194 +1,218 @@
-#!/usr/bin/env python3
-# SPDX-License-Identifier: Apache-2.0
-# Copyright 2019 Germano Gabbianelli <git@germano.dev>
-import glob
-import hashlib
 import logging
-import operator
 import os
 import re
+import subprocess
 import sys
-
-if sys.version_info < (3, 6):
-    sys.stderr.write("Python 3.6 or greater is required\n")
-    sys.exit(1)
-
-from pathlib import Path
-
-if __name__ == "__main__":
-    EXC_PATH = Path(sys.executable).resolve()
-
-    try:
-        _f  # FIXME not solid
-    except NameError:
-        # We are being called with a shebang
-        SRC_PATH = Path(sys.argv[1]).resolve()
-        BIN_PATH = Path(__file__).resolve()
-        EXEC = False
-    else:
-        # We are being `exec`uted inside a script
-        SRC_PATH = Path(sys.argv[0]).resolve()
-        BIN_PATH = SRC_PATH
-        EXEC = True
-
-    try:
-        PYS_HOME = Path(os.environ["PYS_HOME"]).resolve()
-    except KeyError:
-        PYS_HOME = Path.home() / ".local" / "share" / "pysolate"
-
-    try:
-        PYS_VENV = Path(os.environ["PYS_VENV"]).resolve()
-    except KeyError:
-        _hash = hashlib.sha1(str(SRC_PATH).encode("utf-8")).hexdigest()[:6]
-        PYS_VENV = PYS_HOME / "-".join((str(SRC_PATH.name), _hash))
-else:
-    BIN_PATH = Path(sys.argv[0])
+from os import path
 
 
-def restore_var(var):
-    try:
-        os.environ[var] = os.environ[f"_OLD_VIRTUAL_{var}"]
-    except KeyError:
-        pass
+logger = logging.getLogger()
 
 
-def print_env(env=os.environ):
-    for k, v in env.items():
-        if (
-            k.startswith("PYENV")
-            or k in {"PATH", "VIRTUAL_ENV"}
-            or k.startswith("_OLD_VIRTUAL")
-        ):
-            print(k, v)
-
-
-def restart(python="python", bin_path=BIN_PATH):
+def restart(python="python", bin_path=None):
+    bin_path = bin_path or sys.argv[0]
+    logger.debug("       Restarting | %s", python)
     os.execlp(python, python, bin_path, *sys.argv[1:])
 
 
-def parse_version(v: str):
-    return tuple(int(x) for x in v.rsplit("/", 1)[-1].split("."))
+def abspath(x):
+    """
+    Compute absolute path and resolve symlinks
+    """
+    return path.abspath(path.realpath(x))
 
 
-def find_py(version, op=operator.eq):
-    if "PYENV_ROOT" in os.environ:
-        version_t = parse_version(version)
-        PYENV_ROOT = os.environ["PYENV_ROOT"]
+def unpack_ver(v):
+    return tuple(int(x) if x else 0 for x in v)
 
-        for v in glob.glob(f"{PYENV_ROOT}/versions/3.*"):
-            n = parse_version(v)[:2]
-            if op(version_t, n):
-                break
-        else:
-            print("Could not find a suitable python version")
-            sys.exit(1)
 
-        PYTHON = Path(v) / "bin" / "python"
+_fmt = {"alpha": "a", "beta": "b", "candidate": "rc", "final": ""}
+
+
+def format_ver(v):
+    ver = ".".join(map(str, v[:3]))
+    return "%s%s%s" % (ver, _fmt[v[3]], v[4] or "") if len(v) == 5 else ver
+
+
+def find_python_versions():
+    ver_pat = re.compile(r"(\d)\.(\d)(?:\.(\d+).*)?(?:-.*)?$")
+    dirs = map(abspath, os.environ["PATH"].split(":"))
+
+    try:
+        PYENV_ROOT = abspath(os.environ["PYENV_ROOT"])
+    except KeyError:
+        pass
     else:
-        PYTHON = f"python{version}"
+        dir = path.join(PYENV_ROOT, "versions")
 
+        try:
+            entries = os.listdir(dir)
+        except OSError:
+            pass
+        else:
+            for entry in entries:
+                m = ver_pat.match(entry)
+                if m is not None:
+                    yield unpack_ver(m.groups()), path.join(
+                        dir, entry, "bin", "python"
+                    )
+
+        dirs = filter(lambda p: not p.startswith(PYENV_ROOT), dirs)
+
+    exc_pat = re.compile(r"^python(?:\d(?:.\d))$")
+
+    for dir in dirs:
+        try:
+            entries = os.listdir(dir)
+        except OSError:
+            continue
+
+        for entry in filter(exc_pat.match, entries):
+            py = path.join(dir, entry)
+            try:
+                out = subprocess.check_output([py, "-V"])
+            except OSError:
+                continue
+
+            m = ver_pat.search(out.decode("utf-8"))
+            if m is not None:
+                yield unpack_ver(m.groups()), py
+
+
+def find_python():
+    """
+    Finds a Python version compatible with the one requested by the user.
+    If a compatible version cannot be found, it stops pysolate.
+    """
+    try:
+        ver = get_var("python", env=False)
+    except KeyError:
+        logger.debug("  Good Python ver | %s", format_ver(sys.version_info))
+        return sys.executable, sys.version_info
+
+    ver = unpack_ver(ver.split(".")) if isinstance(ver, str) else ver
+    fn = (lambda *v: v[: len(ver)] == ver) if not callable(ver) else ver
+
+    if fn(*sys.version_info[:3]):
+        logger.debug("  Good Python ver | %s", format_ver(sys.version_info))
+        return sys.executable, sys.version_info
+
+    for (v, py) in find_python_versions():
+        if fn(*v):
+            logger.debug(" Found Python ver | %s (%s)", py, format_ver(v))
+            return (py, v)
+
+    logger.error("Could not find matching Python version")
+    sys.exit(1)
+
+
+def run(args, **kwds):
+    if logger.getEffectiveLevel() > 10:
+        kwds["stdin"] = kwds["stdout"] = kwds["stderr"] = subprocess.DEVNULL
+
+    try:
+        subprocess.call(args, **kwds)
+    except OSError:
+        return False
+    else:
+        return True
+
+
+def ensure_venv(venv, dependencies=()):
+    PYTHON = path.join(venv, "bin", "python")
+
+    if not path.exists(venv):
+        py, ver = find_python()
+
+        logger.info("    Creating venv | %s", venv)
+        args = ["--symlinks", venv]
+        if ver < (3, 3):
+            success = (
+                # Try global virtualenv and then with -m as fallback
+                run(["virtualenv", "-p", py] + args)
+                or run([py, "-mvirtualenv", "-p", py] + args)
+            )
+        else:
+            success = run([py, "-mvenv"] + args)
+        if not success:
+            logger.error(
+                "Failed to create venv. Ensure Py > 3.3 or virtualenv is"
+                " installed"
+            )
+        if dependencies:
+            logger.info("  Installing deps")
+            deps = [
+                dep if isinstance(dep, str) else ("%s%s" % dep)
+                for dep in dependencies
+            ]
+            run(
+                [PYTHON, "-mpip", "--disable-pip-version-check", "install"]
+                + deps,
+            )
+
+    os.environ["VIRTUAL_ENV"] = venv
     return PYTHON
 
 
-if __name__ == "__main__":
+def get_var(name, default=None, env=True):
+    try:
+        return globals()[name]
+    except KeyError:
+        if default is not None:
+            name = "PYS_{}".format(name.upper())
+            return os.environ.get(name, default) if env else default
+        raise
 
-    LOG_LEVEL = os.environ.get("PYS_LOGGING", "INFO")
-    LOG_LEVEL = logging.getLevelName(
-        int(LOG_LEVEL) if LOG_LEVEL.isdigit() else LOG_LEVEL.upper()
+
+def main():
+    log_level = get_var("log", "INFO")
+    log_level = logging.getLevelName(
+        int(log_level) if log_level.isdigit() else log_level.upper()
     )
 
     logging.basicConfig(
-        level=LOG_LEVEL,
+        level=log_level,
         format="%(asctime)s %(levelname)5s | %(message)s",
         datefmt="%I:%M:%S",
     )
 
-    logger = logging.getLogger()
-    # logger.debug(f"   sys.executable | {sys.executable}")
+    src = abspath(sys.argv[0])
+    try:
+        home = get_var("home")
+    except KeyError:
+        # TODO: improve
+        home = path.join(path.expanduser("~"), ".local", "share", "pysolate")
 
     try:
-        VIRTUAL_ENV = Path(os.environ["VIRTUAL_ENV"]).resolve()
+        venv = abspath(get_var("venv"))
     except KeyError:
-        logging.debug(f"   No active venv | {sys.executable}")
+        import hashlib
 
-        if not PYS_VENV.exists():
-            logging.info(f"    Creating venv | {PYS_VENV}")
-            import venv
+        _hash = hashlib.sha1(src.encode("utf-8")).hexdigest()[:6]
+        venv = path.join(home, "-".join((path.basename(src), _hash)))
 
-            venv.create(PYS_VENV, symlinks=True, with_pip=True)
-
-        # os.environ["PATH"] = ":".join((str(PYS_VENV / "bin"), os.environ["PATH"]))
-
-        # Activate the correct venv and restart the script
-        os.environ["VIRTUAL_ENV"] = str(PYS_VENV)
-        PYTHON = PYS_VENV / "bin" / "python"
-        restart(PYTHON)
-
+    py = None
+    dependencies = get_var("dependencies", (), env=False)
+    try:
+        current_venv = abspath(os.environ["VIRTUAL_ENV"])
+    except KeyError:
+        logger.debug("   No active venv | %s", sys.executable)
+        py = ensure_venv(venv, dependencies)
     else:
         # We are running inside a wrong virtualenv. Try to exit.
-        if VIRTUAL_ENV != PYS_VENV:
-            logging.debug(f"Wrong active venv | {VIRTUAL_ENV}")
+        if current_venv != venv:
+            logger.debug("Wrong active venv | %s", current_venv)
             del os.environ["VIRTUAL_ENV"]
 
-            if "PYENV_VIRTUAL_ENV" in os.environ:
-                del os.environ["PYENV_VIRTUAL_ENV"]
-
-                os.environ.pop("PYENV_VERSION", None)
-                os.environ.pop("PYENV_DIR", None)
-                os.environ.pop("PYENV_ACTIVATE_SHELL", None)
-
-                PYENV_ROOT = os.environ["PYENV_ROOT"]
-                pattern = (
-                    rf"^[^:]+:[^:]+(:{PYENV_ROOT}/plugins/[^/]+/bin)*:(.*)$"
-                )
-                os.environ["PATH"] = re.match(
-                    pattern, os.environ["PATH"]
-                ).group(2)
-            else:
-                restore_var("PATH")
-                restore_var("PYTHONHOME")
-
-            restart(find_py("3.6", op=operator.le))
+            py = ensure_venv(venv, dependencies)
         else:
-            logger.debug(f" Good active venv | {VIRTUAL_ENV}")
+            logger.debug(" Good active venv | %s", current_venv)
 
-    ### Start script
+    if py is not None:
+        restart(py)
 
-    os.environ["PYS_HOME"] = str(PYS_HOME)
-    os.environ["PYS_VENV"] = str(PYS_VENV)
+    logger.debug("  Starting script | %s", src)
 
-    # TODO: de-initialize logging
 
-    PYS_DEPS = os.environ.get("PYS_DEPS", "")
-
-    if PYS_DEPS:
-        import shlex
-        import subprocess
-
-        logger.debug(f"  Installing deps | {PYS_DEPS}")
-
-        subprocess.check_call(
-            [
-                sys.executable,
-                "-mpip",
-                "-q",
-                "--disable-pip-version-check",
-                "install",
-                *shlex.split(PYS_DEPS),
-            ]
-        )
-        os.environ["PYS_DEPS"] = ""
-
-        logger.debug(f"  Starting script | {SRC_PATH}")
-        restart(sys.executable, SRC_PATH)
-    else:
-
-        if EXEC:
-            # Unset all global variables
-            for var in list(globals().keys()):
-                if not var.startswith("__"):
-                    del globals()[var]
-            del globals()["var"]
-        else:
-            logger.debug(f"  Starting script | {SRC_PATH}")
-            restart(sys.executable, SRC_PATH)
+if "dependencies" in globals():
+    main()
